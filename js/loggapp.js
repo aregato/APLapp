d212e6ef81f474260023030a09e3b7560aeee2e5
hanneslinner <hali93001@utb.vaxjo.se>
2015-11-19 13:18:28 +0100

    APL mockup

---
+++ js/loggapp.js
@@ -0,0 +1,35 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var module = angular.module("LoggApp",["ui.router"]);
+
+module.config(function ($urlRouterProvider,$stateProvider){
+    
+    $urlRouterProvider.otherwise("/");
+    
+    $stateProvider.state("home",{
+        url:"/",
+        template:"<p></p>"
+    }).state("readLoggs",{
+        url:"/read",
+        controller:"readCtrl",
+        templateUrl:"templates/readLoggs.html"
+    }).state("postLogg",{
+        url:"/post",
+        controller:"postCtrl",
+        templateUrl:"templates/postlogg.html"
+    }).state("kontakt",{
+       url:"/kontakt",
+       templateUrl:"templates/kontakt.html"
+    }).state("minasidor",{
+       url:"/minasidor",
+       templateUrl:"templates/minasidor.html"
+    }).state("startsida",{
+        url:"/startsida",
+        templateUrl:"templates/startsida.html"
+    });
+    
+});
+

59564f286e5a0742eb34e041fbfe0a2308fe492b
Carl König <made.swede@live.com>
2015-11-19 14:52:59 +0100

    startsidor fixade

--- js/loggapp.js
+++ js/loggapp.js
@@ -1,8 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 var module = angular.module("LoggApp",["ui.router"]);
 
 module.config(function ($urlRouterProvider,$stateProvider){
@@ -11,7 +6,7 @@ module.config(function ($urlRouterProvider,$stateProvider){
     
     $stateProvider.state("home",{
         url:"/",
-        template:"<p></p>"
+        templateUrl:"templates/" + startsida
     }).state("readLoggs",{
         url:"/read",
         controller:"readCtrl",

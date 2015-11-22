0bf2260ea13fad81164b48e6c81338ae1eefa937
Vamic <made.swede@live.com>
2015-11-22 23:57:06 +0100

    chart.js och enkelt cirkeldiagram

--- js/loggapp.js
+++ js/loggapp.js
@@ -1,71 +1,71 @@
-var module = angular.module("LoggApp",["ui.router"]);
+var module = angular.module("LoggApp", ["ui.router"]);
 
-module.config(function ($urlRouterProvider,$stateProvider){
-    
+module.config(function ($urlRouterProvider, $stateProvider) {
+
     $urlRouterProvider.otherwise("/");
-    
-    $stateProvider.state("home",{
-        url:"/",
-        templateUrl:"templates/" + startsida
-    }).state("readLoggs",{
-        url:"/read",
-        controller:"readCtrl",
-        templateUrl:"templates/readLoggs.html"
-    }).state("postLogg",{
-        url:"/post",
-        controller:"postCtrl",
-        templateUrl:"templates/postlogg.html"
-    }).state("kontakt",{
-       url:"/kontakt",
-       templateUrl:"templates/kontakt.html"
-    }).state("minasidor",{
-       url:"/minasidor",
-       templateUrl:"templates/minasidor.html"
-    }).state("startsida",{
-        url:"/startsida",
-        templateUrl:"templates/startsidaElev.html"
-    }).state("tidslinje",{
-        url:"/tidslinje",
-        templateUrl:"templates/tidslinje.html"
-    }).state("aktivitet",{
-        url:"/aktivitet",
-        templateUrl:"templates/aktivitet.html"
-    }).state("narvaroLarare",{
-        url:"/narvaroLarare",
-        templateUrl:"templates/narvaroLarare.html"
-    }).state("narvaroElev",{
-        url:"/narvaroElev",
-        templateUrl:"templates/narvaroElev.html"
-    }).state("loggbok1",{
-        url:"/loggbok1",
-        templateUrl:"templates/addLogg.html"
-    }).state("loggbok2",{
-        url:"/loggbok2",
-        templateUrl:"templates/loggbok2.html"
-    }).state("momentLarare",{
-        url:"/momentLarare",
-        templateUrl:"templates/momentLarare.html"
-    }).state("momentElev",{
-        url:"/momentElev",
-        templateUrl:"templates/momentElev.html"
-    }).state("loggbocker",{
-        url:"/loggbocker",
-        templateUrl:"templates/loggbocker.html"
-    }).state("oversikt",{
-        url:"/oversikt",
-        templateUrl:"templates/oversikt.html"
-    }).state("meddela",{
-        url:"/meddela",
-        templateUrl:"templates/meddela.html"
-    }).state("semoment",{
-        url:"/semoment",
-        templateUrl:"templates/semoment.html"
-    }).state("lasaloggbok",{
-        url:"/lasaloggbok",
-        templateUrl:"templates/lasaloggbok.html"
+
+    $stateProvider.state("home", {
+        url: "/",
+        templateUrl: "templates/" + startsida
+    }).state("readLoggs", {
+        url: "/read",
+        controller: "readCtrl",
+        templateUrl: "templates/readLoggs.html"
+    }).state("postLogg", {
+        url: "/post",
+        controller: "postCtrl",
+        templateUrl: "templates/postlogg.html"
+    }).state("kontakt", {
+        url: "/kontakt",
+        templateUrl: "templates/kontakt.html"
+    }).state("minasidor", {
+        url: "/minasidor",
+        templateUrl: "templates/minasidor.html"
+    }).state("startsida", {
+        url: "/startsida",
+        templateUrl: "templates/startsidaElev.html"
+    }).state("tidslinje", {
+        url: "/tidslinje",
+        templateUrl: "templates/tidslinje.html"
+    }).state("aktivitet", {
+        url: "/aktivitet",
+        templateUrl: "templates/aktivitet.html"
+    }).state("narvaroLarare", {
+        url: "/narvaroLarare",
+        templateUrl: "templates/narvaroLarare.html"
+    }).state("narvaroElev", {
+        url: "/narvaroElev",
+        templateUrl: "templates/narvaroElev.html"
+    }).state("loggbok1", {
+        url: "/loggbok1",
+        templateUrl: "templates/addLogg.html"
+    }).state("loggbok2", {
+        url: "/loggbok2",
+        templateUrl: "templates/loggbok2.html"
+    }).state("momentLarare", {
+        url: "/momentLarare",
+        templateUrl: "templates/momentLarare.html"
+    }).state("momentElev", {
+        url: "/momentElev",
+        templateUrl: "templates/momentElev.html"
+    }).state("loggbocker", {
+        url: "/loggbocker",
+        templateUrl: "templates/loggbocker.html"
+    }).state("oversikt", {
+        url: "/oversikt",
+        templateUrl: "templates/oversikt.html"
+    }).state("meddela", {
+        url: "/meddela",
+        templateUrl: "templates/meddela.html"
+    }).state("semoment", {
+        url: "/semoment",
+        templateUrl: "templates/semoment.html"
+    }).state("lasaloggbok", {
+        url: "/lasaloggbok",
+        templateUrl: "templates/lasaloggbok.html"
 
     });
-    
+
 });
 
 module.controller("aktivitetsCtrl", function ($scope) {
@@ -83,15 +83,15 @@ module.controller("aktivitetsCtrl", function ($scope) {
         }];
 });
 
-module.controller("urlCtrl", function ($scope){
-    $scope.changeUrl = function (){
+module.controller("urlCtrl", function ($scope) {
+    $scope.changeUrl = function () {
         var indata = $scope.loginname.toLowerCase();
         console.log(indata);
-        if(indata === "elev"){
+        if (indata === "elev") {
             $scope.urlValue = "elev.html";
-        }else if(indata === "lärare"){
+        } else if (indata === "lärare") {
             $scope.urlValue = "larare.html";
-        }else if(indata === "handledare"){
+        } else if (indata === "handledare") {
             $scope.urlValue = "handledare.html";
         }
         $scope.loginname = "";
